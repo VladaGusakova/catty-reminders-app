2c0e034f8d46bfd5394e6ee8338f554f4140e9d8
VladaGusakova <vlada0gusakova@gmail.com>
2026-05-04 03:35:20 +0500

    fix33

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,27 @@
 set -e
 
 TARGET_DIR="/home/pass1234/Desktop/DevOps/catty-reminders-app"
-IMAGE_NAME="ghcr.io/vladagusakova/catty-reminders-app:latest"
+COMMIT_SHA=$1
 
-COMMIT_SHA=$2
-
-echo "Переходим в директорию $TARGET_DIR..."
-cd "$TARGET_DIR"
-
-touch .env
-if [ -n "$COMMIT_SHA" ] && [ "$COMMIT_SHA" != "unknown" ]; then
-    echo "DEPLOY_REF=$COMMIT_SHA" > .env
+if [ -z "$COMMIT_SHA" ] || [ "$COMMIT_SHA" == "unknown" ]; then
+    echo "Ошибка: COMMIT_SHA не передан!"
+    exit 1
 fi
 
-echo "Скачиваем новый Docker-образ..."
+IMAGE_NAME="ghcr.io/vladagusakova/catty-reminders-app:${COMMIT_SHA}"
+
+echo "Переходим в директорию $TARGET_DIR..."
+mkdir -p "$TARGET_DIR"
+cd "$TARGET_DIR"
+
+echo "Скачиваем новый Docker-образ по хэшу: $IMAGE_NAME"
 docker pull $IMAGE_NAME
 
 echo "Останавливаем и удаляем старый контейнер (если есть)..."
+docker stop catty-app-container || true
 docker rm -f catty-app-container || true
 
-echo "Запускаем новый контейнер с пробросом файла .env..."
-docker run -d -p 8181:8181 --env-file .env --name catty-app-container  --restart always $IMAGE_NAME
+echo "Запускаем новый контейнер с прямой передачей переменной..."
+docker run -d --name catty-app-container --restart always -p 8181:8181 -e DEPLOY_REF="$COMMIT_SHA" $IMAGE_NAME
 
 echo "Развертывание через Docker завершено успешно!"
