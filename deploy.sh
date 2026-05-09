6a056800fc0f4adad31080effa60acf678c84f16
VladaGusakova <163380239+VladaGusakova@users.noreply.github.com>
2026-05-09 22:26:44 +0400

    fix deploy.sh
    Moved test execution to the end of the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,6 @@ git fetch origin "$BRANCH"
 git checkout "$BRANCH"
 git reset --hard "origin/$BRANCH"
 
-echo "Запускаем тесты..."
-./test.sh
-
 echo "Записываем хэш коммита в .env..."
 if [ -z "$COMMIT_SHA" ] || [ "$COMMIT_SHA" == "unknown" ]; then
     COMMIT_SHA=$(git rev-parse HEAD)
@@ -29,5 +26,10 @@ pip install -r requirements.txt
 
 echo "Перезапускаем системную службу uvicorn..."
 sudo systemctl restart catty-app
+
+sleep 5
+
+echo "Запускаем тесты..."
+./test.sh
 
 echo "Развертывание завершено успешно!"
